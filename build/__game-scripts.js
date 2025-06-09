ad5a121dc4a11a78a263adab93f172e4d2234568
Alex <voronko20172017@gmail.com>
2025-06-09 19:39:15 +0300

    Update __game-scripts.js

--- build/__game-scripts.js
+++ build/__game-scripts.js
@@ -196,8 +196,8 @@ Cell.attributes.add("spriteObj", {
 var LevelManager = pc.createScript("levelManager");
 LevelManager.instance = null, LevelManager.prototype.initialize = function () {
     if (LevelManager.instance) return console.warn("LevelManager already exists"), void this.entity.destroy();
-    LevelManager.instance = this, this.currentLevel = 0, this.firstClickedCell = null, this.completed = !1, this.camera = this.app.root.findByName("Camera"), this.nextCount = 0, this.btn = this.app.root.findByName("Btn"), this.btn && this.btn.element && (this.btn.element.useInput = !0, this.btn.element.interactable = !0, console.log("Кнопка найдена: " + this.btn), this.btn.element.on("mousedown", function (e) {
-        console.log("Нажата кнопка: " + this.name)
+    LevelManager.instance = this, this.currentLevel = 0, this.firstClickedCell = null, this.completed = !1, this.camera = this.app.root.findByName("Camera"), this.nextCount = 0, this.btn = this.app.root.findByName("Btn"), this.btn && this.btn.element && (this.btn.element.useInput = !0, this.btn.element.interactable = !0, console.log("Кнопка найдена: " + this.btn), this.btn.button.on("click", function (e) {
+        console.log("Нажата кнопка: ")
     }, this)), this.app.mouse.on(pc.EVENT_MOUSEDOWN, this.handleClick, this)
 }, LevelManager.prototype.handleClick = function (e) {
     if (this.completed) return;
